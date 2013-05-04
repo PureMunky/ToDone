78e4ffae732481f37f3fbbb6139165e5e160bdf9
Phil Corbett <corbett.phil@gmail.com>
2013-05-03 22:43:33 -0400

    setup partials an route providers

--- js/core.js
+++ js/core.js
@@ -1,5 +1,18 @@
-function ListCtrl($scope, $http) {
+angular.module('ToDone', [])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/list', {templateUrl: 'partials/TodoList.htm', controller: TodoListCtrl})
+			.when('/edit/:TodoID', {templateUrl: 'partials/TodoEdit.htm', controler: TodoEditCtrl})
+			.otherwise({redirectTo: '/list'});
+	}]);
+
+function TodoListCtrl($scope, $http) {
 	$http.get('data/todos.json').success(function(data) {
 		$scope.todos = data;
 	});
+}
+
+function TodoEditCtrl($scope, $routeParams) {
+	$scope.id = $routeParams.TodoID;
+	// TODO: seems to not pass the id correctly.
 }
