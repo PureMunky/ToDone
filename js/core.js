94172022f34764d9a58c8b2b9e7c302f805773d6
corbett.phil@gmail.com <>
2013-06-16 20:54:12 +0000

    current list controllers

--- js/core.js
+++ js/core.js
@@ -4,32 +4,48 @@ var API = {
 	TodoList: 'data/todos.json'
 };
 
+var ToDone = {};
+
 /* Angular App */
-angular.module('ToDone', [])
+ToDone.App = angular.module('ToDone', [])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
-			.when('/list', {templateUrl: 'partials/TodoList.htm', controller: TodoListCtrl})
-			.when('/edit/:TodoID', {templateUrl: 'partials/TodoEdit.htm', controller: TodoEditCtrl})
+			.when('/list', {templateUrl: 'partials/TodoList.htm', controller: ToDone.Controllers.TodoList})
+			.when('/edit/:TodoID', {templateUrl: 'partials/TodoEdit.htm', controller: ToDone.Controllers.TodoEdit})
+      .when('/current', {templateUrl: 'partials/CurrentList.htm', controller: ToDone.Controllers.CurrentList})
 			.otherwise({redirectTo: '/list'});
 	}]);
 
 /* Angular Controllers */
-function TodoListCtrl($scope, $http) {
-	$http.get(API.TodoList).success(function(data) {
-		$scope.todos = data;
-	});
-}
-
-function TodoEditCtrl($scope, $http, $routeParams) {
-	$http.get(API.TodoEdit).success(function(data) {
-		$scope.todo = data;
-	});
-}
+ToDone.Controllers = (function() {
+  var that = {};
+  
+  that.TodoList = function($scope, $http) {
+  	$http.get(API.TodoList).success(function(data) {
+  		$scope.todos = data;
+  	});
+  };
+  
+  that.CurrentList = function ($scope, $http) {
+    $http.get(API.CurrentList)
+  };
+  
+  that.TodoEdit = function ($scope, $http, $routeParams) {
+  	$http.get(API.TodoEdit).success(function(data) {
+  		$scope.todo = data;
+  	});
+  }
+  
+  return that;
+})();
 
 /* Angular Modules */
 angular.module('task', [])
   .directive('addTask', function() {
     return {
-      templateUrl: 'partials/directives/addTask.htm'
+      templateUrl: 'partials/directives/addTask.htm',
+      compile: function compile(tElement, tAttrs) {
+        return function postLink(scope, iElement, iAttrs) {}
+      }
     }
   });
