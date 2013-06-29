5ffaf89bab64d15e4180e2b140d0574d82387345
Phil Corbett <corbett.phil@gmail.com>
2013-06-29 13:05:30 -0400

    create json filter

--- js/filters.js
+++ js/filters.js
@@ -1,1 +1,20 @@
-﻿
+﻿ToDone.App.filter('jsonDate', function () {
+    return function (input, format) {
+
+        // Exit if the value isn't defined
+        if (angular.isUndefined(input)) {
+            return;
+        }
+
+        var date = new Date(parseInt(input.substr(6)));
+
+        // John Pedrie added Moment.js support
+        if (typeof moment !== 'undefined' && format) {
+            var momentObj = moment(date);
+            return momentObj.format(format);
+        }
+        else {
+            return date.toLocaleDateString();
+        }
+    }
+});
