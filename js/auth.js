6b9e6fee9068042cf215f18fe9a4dd2fc891394c
Phil Corbett <corbett.phil@gmail.com>
2013-08-25 09:50:33 -0400

    update the signin stuff....maybe this time

--- js/auth.js
+++ js/auth.js
@@ -1,20 +1,14 @@
-var Auth = (function () {
-   var that = {};
-   
-   that.GoogleSignIn = function(authResult) {
-       if (authResult['access_token']) {
-            // Successfully authorized
-            // Hide the sign-in button now that the user is authorized, for example:
-            document.getElementById('signinButton').setAttribute('style', 'display: none');
-            console.log('Signed in!!');
-        } else if (authResult['error']) {
-            // There was an error.
-            // Possible error codes:
-            //   "access_denied" - User denied access to your app
-            //   "immediate_failed" - Could not automatically log in the user
-            console.log('There was an error: ' + authResult['error']);
-        }
-   };
-   
-   return that;
-})();
+function GoogleSignIn(authResult) {
+   if (authResult['access_token']) {
+        // Successfully authorized
+        // Hide the sign-in button now that the user is authorized, for example:
+        document.getElementById('signinButton').setAttribute('style', 'display: none');
+        console.log('Signed in!!');
+    } else if (authResult['error']) {
+        // There was an error.
+        // Possible error codes:
+        //   "access_denied" - User denied access to your app
+        //   "immediate_failed" - Could not automatically log in the user
+        console.log('There was an error: ' + authResult['error']);
+    }
+}
