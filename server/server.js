d5320b555ced99819fc51f843b995175db429c35
Phil Corbett <corbett.phil@gmail.com>
2013-09-05 06:42:11 -0400

    starting to build the node server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = require('url');
+
+function start() {
+  function onRequest(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    console.log('Request for ' + pathname + ' recieved');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('hello!!');
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server Started');
+}
+
+exports.start = start;
