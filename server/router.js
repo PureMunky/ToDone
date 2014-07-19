8800b2a3f7871923d4791272cdaade350a7f3166
Phil Corbett <corbett.phil@gmail.com>
2014-07-19 11:54:37 -0400

    create a router

---
+++ server/router.js
@@ -0,0 +1,5 @@
+function route(pathname) {
+  console.log('about to route a request for ' + pathname);
+}
+
+exports.route = route;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 var http = require('http');
 var url = require('url');
 
-function start() {
+function start(route) {
   function onRequest(req, res) {
     var pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' recieved');
+
+    route(pathname);
+
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('yep, you just found my node server! please be nice!! :D');
   }
 
-  http.createServer(onRequest).listen(80);
+  http.createServer(onRequest).listen(1337);
   console.log('Server Started');
 }
 
