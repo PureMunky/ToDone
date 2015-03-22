1a9663efadcf55776a15c62b315b95e724809010
Phil Corbett <corbett.phil@gmail.com>
2015-03-22 14:39:49 -0400

    authenticate todo api

--- server/todo/todoRoute.js
+++ server/todo/todoRoute.js
@@ -7,12 +7,14 @@ var rh = require('../routeHelper.js');
 var user = require('../user/userCtrl.js');
 
 router.get('/', function (req, res, next) {
-  todo.find(rh.resolve(res, next));
+  rh.authenticate(req, res, next, function (err, userId) {
+    todo.find({ _owner: userId }, rh.resolve(res, next));
+  });
 });
 
 router.get('/list/:sort', function (req, res, next) {
   rh.authenticate(req, res, next, function (err, userId) {
-      todo.find({ _owner: userId }).populate('Tags').sort({ title: 1 }).exec(rh.resolve(res, next));
+    todo.find({ _owner: userId }).populate('Tags').sort({ title: 1 }).exec(rh.resolve(res, next));
   });
 });
 
@@ -26,19 +28,23 @@ router.get('/:id', function (req, res, next) {
       Description: ''
     }));
   } else {
-    todo.findById(req.params.id).populate('Tags').exec(rh.resolve(res, next));
+    rh.authenticate(req, res, next, function (err, userId) {
+      todo.findOne({ _id: req.params.id, _owner: userId }).populate('Tags').exec(rh.resolve(res, next));
+    });
   }
 });
 
 router.put('/:id', function (req, res, next) {
-  todo.update({ _id: req.params.id }, req.body, rh.resolve(res, next));
+  rh.authenticate(req, res, next, function (err, userId) {
+    req.body._owner = userId;
+    todo.update({ _id: req.params.id, _owner: userId }, req.body, rh.resolve(res, next));
+  });
 });
 
 router.post('/', function (req, res, next) {
   rh.authenticate(req, res, next, function (err, userId) {
-      var task = req.body;
-      task._owner = userId;
-      todo.create(task, rh.resolve(res, next));
+    req.body._owner = userId;
+    todo.create(req.body, rh.resolve(res, next));
   });
 });
 
