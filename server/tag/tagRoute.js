16b3d334611e427822b65e0973e2dabc05c0b661
Phil Corbett <corbett.phil@gmail.com>
2015-03-22 14:39:25 -0400

    authenticate tag api

--- server/tag/tagRoute.js
+++ server/tag/tagRoute.js
@@ -6,25 +6,34 @@ var tag = require('./tagModel.js');
 var rh = require('../routeHelper.js');
 
 router.get('/', function (req, res, next) {
-  tag.find(rh.resolve(res, next));
+  rh.authenticate(req, res, next, function (err, userId) {
+    tag.find({ _owner: userId }, rh.resolve(res, next));
+  });
 });
 
 router.get('/type/:typeId', function (req, res, next) {
-  tag.find({ Type: req.params.typeId }, rh.resolve(res, next));
+  rh.authenticate(req, res, next, function (err, userId) {
+    tag.find({ _owner: userId, Type: req.params.typeId }, rh.resolve(res, next));
+  });
 });
 
 router.put('/:id', function (req, res, next) {
-  tag.findByIdAndUpdate(req.params.id, req.body, rh.resolve(res, next));
+  rh.authenticate(req, res, next, function (err, userId) {
+    req.body._owner = userId;
+    tag.update({ _id: req.params.id, _owner: userId }, req.body, rh.resolve(res, next));
+  });
 });
 
 router.post('/', function (req, res, next) {
-  tag.findOne({ Title: req.body.Title }).exec(function (err, data) {
-    console.log(data);
-    if (data != undefined) {
-      rh.resolve(res, next)(null, data);
-    } else {
-      tag.create(req.body, rh.resolve(res, next));
-    }
+  rh.authenticate(req, res, next, function (err, userId) {
+    tag.findOne({ _owner: userId, Title: req.body.Title }).exec(function (err, data) {
+      if (data != undefined) {
+        rh.resolve(res, next)(null, data);
+      } else {
+        req.body._owner = userId;
+        tag.create(req.body, rh.resolve(res, next));
+      }
+    });
   });
 });
 
