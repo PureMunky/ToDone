0fc015e3d046dd2372988e8c72d3ecf625f1dc7c
Phil Corbett <corbett.phil@gmail.com>
2015-03-22 09:08:26 -0400

    testing user key generation

--- spec/userCtrl-spec.js
+++ spec/userCtrl-spec.js
@@ -1,8 +1,18 @@
 ﻿'use strict';
 
-var user = require('../server/user/userCtrl.js');
+var user = require('../server/user/userCtrl.js'),
+  userModel = require('../server/user/userModel.js'),
+  mongoose = require('mongoose');
+
 
 describe('userCtrl.js', function () {
+  beforeEach(function (done) {
+    mongoose.connect('mongodb://localhost/todoTest', function () {
+      userModel.remove({}, false).exec(function () {
+        done();
+      });
+    });
+  });
 
   it('works', function () {
     expect(1).toBe(1);
@@ -15,13 +25,79 @@ describe('userCtrl.js', function () {
     expect(newKey.length).toBe(128);
   });
 
-  it('gets user ids from keys', function () {
-    var key = user.getKey('hello'),
-      userId = user.getID(key),
-      newKey = user.getKey(key),
-      newUserId = user.getID(newKey);
+  it('gets user ids from keys', function (done) {
+    var tKey, tnewKey, tUserId, tNewUserId;
 
-    expect(userId).toBe(newUserId);
+    // Get a brand new key.
+    user.getKey('hello', testFirstKey);
+
+    // Test the initial key generation.
+    function testFirstKey(err, key) {
+      expect(key).not.toBe('hello');
+      expect(key).toBeDefined();
+      tKey = key;
+      user.getID(key, testUserId);
+    }
+
+    // Test the user id that comes back from the first key.
+    function testUserId(err, userId) {
+      expect(userId).toBeDefined();
+      tUserId = userId;
+      user.getKey(tKey, testSecondKey)
+    }
+
+    // Test the second key generated from the first key.
+    function testSecondKey(err, newKey) {
+      expect(newKey).not.toBe(tKey);
+      tnewKey = newKey;
+      user.getID(newKey, testSecondUserId);
+    }
+
+    // Test that the first user id matches the user id returned from
+    // the second key.
+    function testSecondUserId(err, newUserId) {
+      expect(newUserId.id).toBe(tUserId.id);
+      tNewUserId = newUserId;
+      done();
+    }
+    
+  });
+
+  it('doesn\'t store initial key attempt', function (done) {
+    var initialKey, tFirstKey, tSecondKey, tFirstUserId, tSecondUserId;
+
+    initialKey = 'hello';
+
+    user.getKey(initialKey, testFirstKey);
+
+    function testFirstKey(err, firstKey) {
+      tFirstKey = firstKey;
+      expect(firstKey).toBeDefined();
+
+      user.getID(firstKey, testFirstUserId);
+    }
+
+    function testFirstUserId(err, firstUserId) {
+      tFirstUserId = firstUserId;
+      expect(firstUserId).toBeDefined();
+
+      user.getKey(initialKey, testSecondKey);
+    }
+
+    function testSecondKey(err, secondKey) {
+      tSecondKey = secondKey;
+      expect(secondKey).toBeDefined();
+      expect(secondKey).not.toBe(tFirstKey);
+
+      user.getID(secondKey, testSecondUserId);
+    }
+
+    function testSecondUserId(err, secondUserId) {
+      tSecondUserId = secondUserId;
+      expect(testFirstUserId.id).not.toBe(secondUserId.id);
+      done();
+    }
+
   });
 
 });
