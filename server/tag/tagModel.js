e8f5773355c8b381659833aa1ceca8969ff94b72
Phil Corbett <corbett.phil@gmail.com>
2015-03-22 14:40:12 -0400

    prevent owner ids from displaying on tags and todos

--- server/tag/tagModel.js
+++ server/tag/tagModel.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var TagSchema = new Schema({
-  _owner: { type: Schema.Types.ObjectId, ref: 'User' },
+  _owner: { type: Schema.Types.ObjectId, ref: 'User', select: false},
   Title: String,
   Type: Number,
 });
