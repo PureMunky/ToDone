25ac7e31c0db8168838af0e51ef81b2b88a2c453
Phil Corbett <corbett.phil@gmail.com>
2015-03-29 10:50:09 -0400

    make dates pretty

--- js/filters.js
+++ js/filters.js
@@ -1,27 +1,4 @@
-ToDone.App.filter('jsonDate', function () {
-    return function (input, format) {
-
-        // Exit if the value isn't defined
-        if (angular.isUndefined(input)) {
-            return;
-        }
-
-        if (!input) {
-            return '';
-        }
-
-        var date = new Date(parseInt(input.substr(6)));
-
-        // John Pedrie added Moment.js support
-        if (typeof moment !== 'undefined' && format) {
-            var momentObj = moment(date);
-            return momentObj.format(format);
-        }
-        else {
-            return date.toLocaleDateString();
-        }
-    }
-}).filter('linkify', function () {
+ToDone.App.filter('linkify', function () {
     return function (input) {
         var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
     
