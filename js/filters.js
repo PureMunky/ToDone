3130e864af823c157cec2256769b333b51f26f65
Phil Corbett <corbett.phil@gmail.com>
2015-04-04 12:09:38 -0400

    more countdown

--- js/filters.js
+++ js/filters.js
@@ -1,7 +1,18 @@
 ToDone.App.filter('linkify', function () {
-    return function (input) {
-        var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    
-        return input ? input.replace(exp,"<a href='$1' target='_blank'>$1</a>") : ''; 
-    };
+  return function (input) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+
+    return input ? input.replace(exp, "<a href='$1' target='_blank'>$1</a>") : '';
+  };
+});
+
+ToDone.App.filter('until', function () {
+  return function (input) {
+    var toDate = new Date(input),
+      fromDate = new Date(),
+      mili = (toDate - fromDate),
+      days = Math.round((mili / (1000 * 60 * 60 * 24)) + 0.5, 0);
+
+    return days + ' days';
+  };
 });
